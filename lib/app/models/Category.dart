662a41c2517aec5234ed2097205f7fb489e5d62f
vlotysh <lotysh.vm@gmail.com>
2020-09-27 23:25:37 +0300

    Updated code style

--- lib/app/models/Category.dart
+++ lib/app/models/Category.dart
@@ -5,5 +5,6 @@ class Category {
   final String title;
   final Color color;
 
-  const Category({@required this.id, @required this.title, @required this.color});
+  const Category(
+      {@required this.id, @required this.title, @required this.color});
 }
